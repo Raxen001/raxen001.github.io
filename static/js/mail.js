ed3261cea25d77cd559f806694709e7793e42b8f
raxen001 <raxen001@tutamail.com>
2023-08-08 21:27:54 +0530

    added alert box in mailjs

--- static/js/mail.js
+++ static/js/mail.js
@@ -5,8 +5,9 @@ window.onload = function () {
 			event.preventDefault();
 			emailjs.sendForm("service_c0lkcza", "template_4xv57y7", this).then(
 				function () {
-					console.log("Mail Sent Succesfully!");
 					document.getElementById("contact_form").reset();
+					console.log("Mail Sent Succesfully!");
+					alert("Mail Sent Successfully!. Will get back to you shortly...");
 				},
 				function (error) {
 					console.log("MAIL FAILED...", error);
