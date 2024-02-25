e4ae95bea191d82e04b13d0ba775053b6276ee51
RITHESH S <raxen001@tutamail.com>
2024-02-25 20:10:33 +0530

    changed the front page, added listenbrainz, customizing contacts

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
-IP="$(hostname -I | awk '{print $1}')"
+#IP="$(hostname -I | awk '{print $1}')"
+IP="0.0.0.0"
 xdg-open "http://$IP:1313"
 hugo serve -D --navigateToChanged --bind "$IP" --baseURL "http://$IP/"
